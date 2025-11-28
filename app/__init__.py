e714821c6457cb20d273ca8e053354ba7313de41
lamelihuynh <gianglinh271@gmail.com>
2025-11-28 21:10:23 +0700

    Notification module and some add to Schedule module to make trigger send notification when create/update/delete schedule - v2

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,9 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
     from app.modules.schedule.scheduleRoutes import schedule_bp
     app.register_blueprint(schedule_bp, url_prefix='/schedule')
+    
+    from app.modules.notification.routes import notification_bp
+    app.register_blueprint(notification_bp, url_prefix='/notification')
 
     # Future modules (Session, Report, etc.) will be registered here
     # from app.modules.session.routes import session_bp
